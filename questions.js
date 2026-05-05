ff89c68af4a3d7ddae4288dfa585c1618252f3a9
Izzatulloh Eshimboyev <eshimboevizzatullos@gmail.com>
2026-05-05 09:14:48 +0500

    two issue worked

---
+++ questions.js
@@ -0,0 +1,14 @@
+// Hamma masala shu array asosida ishlanadi
+// 1-masala
+// Function yasang unda yuqoridagi arrayni price 500 dan yuqorilarini bitta arrayga yegib
+// qaytarsin.
+// 2-masala
+// Function yasang unda yuqoridagi arrayni rating 4dan yuqorilarini bitta arrayga yegib qaytarsin.
+// 3-masala
+// Function yasang unda yuqoridagi arrayda nechta mahsulot borligi aniqlansin va qaytarsin
+// 4-masala
+// Function yasang unda yuqoridagi arrayni price 500 dan yuqorilariga 10% lik chegirma berilsin
+// 500dan pastlariga 10% miqdorda narx qoshilsin va yangiangan array qayt sin qaytarsin.
+// 5-masala
+// Functuon yozing yuqoridagi arr ni clothes categoryga oidlarini olib tashlang va category Clothing
+// bolganlarsiz array qaytaring
